7b3021cc4521d527898530dff5af848c24c70a68
nispap-ai <nispap@gmail.com>
2026-08-16 00:01:00 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
 const CACHE_NAME = 'my-pwa-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/icon-192.png'
+  './',
+  './index.html',
+  './app.js',
+  './icon-192.png'
 ];
 
 // ক্যাশ সেভ করা
